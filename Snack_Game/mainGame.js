96550388afcf9f882aee1eaacc86e80ff6db69a7
vrowdice <minmin310342@gmail.com>
2022-10-03 06:42:43 +0000

    main

--- Snack_Game/mainGame.js
+++ Snack_Game/mainGame.js
@@ -1,31 +1,48 @@
-var mainEl = document.getElementById("mainEl");
-var canvasParant = document.getElementById("canvasParant")
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const mainEl = document.getElementById("mainEl");
+const canvasParant = document.getElementById("canvasParant")
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const maxMap = 50;
+const minMap = 5;
+
+const maxSpeed = 10;
+const minSpeed = 0.1
 
 var mapX = 0;
 var mapY = 0;
+
+var gameSpeed = 0;
+
+var map = new Array();
 
 var isGameStart = false;
 
 function ClickMainBtn(){
     mapX = document.getElementById("inputX").value;
     mapY = document.getElementById("inputY").value;
+    gameSpeed = document.getElementById("inputS").value;
     
-    if(mapX >= 10 && mapX <= 200 &&
-      mapY >= 10 && mapY <= 200){
+    if(mapX >= minMap && mapX <= maxMap &&
+      mapY >= minMap && mapY <= maxMap &&
+      gameSpeed >= minSpeed && gameSpeed <= maxSpeed){
         isGameStart = true;
-        if(isGameStart){
-            canvasParant.style.display = 'block';
-            mainEl.style.display = 'none';
-            setInterval(MainGame, 10);
-        }
-        else{
-            canvasParant.style.display = 'none';
-            mainEl.style.display = 'block';
+        
+        canvasParant.style.display = 'block';
+        mainEl.style.display = 'none';
+        setInterval(MainGame, gameSpeed * 100);
+        
+        canvas.width = mapX * 40 + 5
+        canvas.height = mapY * 40 + 5
+        
+        for (var i = 0; i < mapX; i++) {
+    		arr[i] = new Array(2);
         }
     }
     else{
+        canvasParant.style.display = 'none';
+        mainEl.style.display = 'block';
+        
         alert("Pleas enter right value...")
         return;
     }
@@ -33,14 +50,22 @@ function ClickMainBtn(){
 
 function MainGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     ctx.beginPath();
-    ctx.arc(mapX, mapY, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
+    
+    AddMap();
     
     ctx.closePath();
-    mapX += 1;
-    mapY += 1;
+}
+
+function AddMap(){
+    for(var i = 0; i < mapX; i++){
+    	for(var o = 0; o < mapY; o++){
+        	ctx.fillRect(5 + i * 40, 5 + o * 40, 35, 35);
+    		ctx.fillStyle = "#BBBBBB";
+    		ctx.fill();
+            
+            console.debug(o);
+    	}
+    }
 }
 
